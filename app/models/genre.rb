075c34b21f6b104052b60b60240e266c10b68703
Rico Mitchell <ricomitch@icloud.com>
2020-04-14 20:08:58 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,17 +3,17 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    
     self.song.length
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+
     self.artist.length
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
-    self.artist.map(&:name) 
+
+    self.artist.map(&:name)
   end
 end
